c1eb8c374bfb5ff3014d34c94dc40e15bb2cb15b
Fraenze-Thinkpad <fraenzest@yahoo.de>
2019-09-16 13:30:59 +0200

    responsive navbar

--- JavaScript/appIndexPage.js
+++ JavaScript/appIndexPage.js
@@ -1,1 +1,31 @@
-$(document).ready(function () {});
+// const navSlide = () => {
+const burger = document.querySelector(".navbar-burger");
+const nav = document.querySelector('.navbar-links');
+const navLinks = document.querySelectorAll('.nb-right');
+
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = ''
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+//             }
+//         });
+
+//     });
+// }
+
+
+// navSlide();
+
+// $('.navbar-burger').click(function () {
+//     if ($(".navbar-links").is(':visible')) {
+//         $('.dropdown-content').slideUp();
+//     }
+//     $(".navbar-links").slideToggle();
+// });
+
+$('.navbar-burger').click(function () {
+    $('navbar-links').slideToggle();
+});
