60f4164561e6c7d59023317c50414612f3593857
fraenze-st <fraenzest@yahoo.de>
2019-09-21 20:26:42 +0200

    jekyll functionality

--- _site/assets/JavaScript/appIndexPage.js
+++ _site/assets/JavaScript/appIndexPage.js
@@ -14,17 +14,17 @@
 
 
 
-// navbar active-class
-var navProjects = document.querySelectorAll(".nbpp-link");
-// Loop through the links and add the active class to the current/clicked link
-for (var i = 0; i < navProjects.length; i++) {
-    navProjects[i].addEventListener("click", function () {
-        console.log("clicked");
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+// // navbar active-class
+// var navProjects = document.querySelectorAll(".nbpp-link");
+// // Loop through the links and add the active class to the current/clicked link
+// for (var i = 0; i < navProjects.length; i++) {
+//     navProjects[i].addEventListener("click", function () {
+//         console.log("clicked");
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
 
 
 
